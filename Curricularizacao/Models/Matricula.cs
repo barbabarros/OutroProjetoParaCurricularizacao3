8f8ce933461e4a132bb7f7442ee02426a98b047f
barbabarros <barbabarros@windowslive.com>
2024-10-19 07:18:48 -0300

    Correcao da Classe Matricula

--- Curricularizacao/Models/Matricula.cs
+++ Curricularizacao/Models/Matricula.cs
@@ -10,6 +10,8 @@ public class Matricula
     public int Id { get; set; }
 
     public Aluno? Aluno { get; set; }
+    public int AlunoId { get; set; }
 
     public Turma? Turma { get; set; }
+    public int TurmaId { get; set; }
 }
