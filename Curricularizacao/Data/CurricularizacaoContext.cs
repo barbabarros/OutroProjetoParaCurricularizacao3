0d1a498cabca8263ce2b80a304a88b6a9142bc4b
andrebarbosaalvesdossantos <andrebarbosaalvesdossantos@gmail.com>
2024-11-05 20:06:09 -0300

    persistencia

---
+++ Curricularizacao/Data/CurricularizacaoContext.cs
@@ -0,0 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+using Curricularizacao.Models;
+
+namespace Curricularizacao.Data
+{
+    public class CurricularizacaoContext(DbContextOptions<CurricularizacaoContext> options) : DbContext(options)
+    {
+        public DbSet<Aluno>? Alunos { get; set; }
+        public DbSet<Professor>? Professores { get; set; }
+        public DbSet<Atividade>? Atividades { get; set; }
+        public DbSet<Matricula>? Matriculas { get; set; }
+    }
+}
